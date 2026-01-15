7b87aeb21f16a3adb4e428ab9d608e5613526b23
coderasad <coredeveloper.2013@gmail.com>
2026-01-15 20:09:47 +0600

    Update specialistSection.tsx

--- app/components/sections/specialistSection.tsx
+++ app/components/sections/specialistSection.tsx
@@ -4,10 +4,10 @@ import bgImage from "@/assets/images/bg-ready.png";
 const SpecialistSection = () => {
     return (
         <div className="bg-cover bg-no-repeat bg-top pt-20 pb-24 relative"
-             style={{ backgroundImage: `url(${bgImage.src})` }}
+             // style={{ backgroundImage: `url(${bgImage.src})` }}
         >
             <div>
-                Specialist Section
+
             </div>
         </div>
     );
