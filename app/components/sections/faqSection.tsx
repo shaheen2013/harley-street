594fd226e895547a4822e28551dd76cfe82e64d0
coderasad <coredeveloper.2013@gmail.com>
2026-01-16 05:44:01 +0600

    Update faqSection.tsx

--- app/components/sections/faqSection.tsx
+++ app/components/sections/faqSection.tsx
@@ -52,7 +52,29 @@ const FaqSection = () => {
                         </div>
                     </div>
                 </div>
-                <div></div>
+                <div className="flex flex-col gap-3">
+
+                    <div className="flex flex-col gap-3">
+                        <div className="bg-white rounded-xl shadow-[0px_10px_80px_0px_#A8AEB733] p-5 flex justify-between items-center">
+                            <div className="text-xl font-semibold leading-7.5 line-clamp-1">Will my information be secure?</div>
+                            <div className=" bg-[#F1F5F9] w-8 h-8 rounded-full p-1 flex-center cursor-pointer"><HiOutlinePlus /></div>
+                        </div>
+                        <div className="hidden bg-white rounded-xl shadow-[0px_10px_80px_0px_#A8AEB733] p-5">
+                            <div className="text-lg leading-[150%] line-clamp-3">Yes. We use secure, encrypted, GDPR-compliant systems for the transfer and storage of medical images and reports. Access is restricted to the team directly involved in your care.</div>
+                        </div>
+                    </div>
+
+                    <div className="flex flex-col gap-3">
+                        <div className="bg-white rounded-xl shadow-[0px_10px_80px_0px_#A8AEB733] p-5 flex justify-between items-center">
+                            <div className="text-xl font-semibold leading-7.5 line-clamp-1">Can I share the report with my doctor?</div>
+                            <div className=" bg-[#F1F5F9] w-8 h-8 rounded-full p-1 flex-center cursor-pointer"><HiOutlinePlus /></div>
+                        </div>
+                        <div className="hidden bg-white rounded-xl shadow-[0px_10px_80px_0px_#A8AEB733] p-5">
+                            <div className="text-lg leading-[150%] line-clamp-3">Yes. We use secure, encrypted, GDPR-compliant systems for the transfer and storage of medical images and reports. Access is restricted to the team directly involved in your care.</div>
+                        </div>
+                    </div>
+                </div>
+
             </div>
         </div>
     );
