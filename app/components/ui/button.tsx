f2c40dafe9eb052fff4fe3a77edf945b7a55c9a5
coderasad <coredeveloper.2013@gmail.com>
2026-01-16 18:50:01 +0600

    Add bgColor prop to Button and update usage

--- app/components/ui/button.tsx
+++ app/components/ui/button.tsx
@@ -4,17 +4,18 @@ interface ButtonProps {
     title: string;
     type?: 'outline';
     large?: boolean;
+    bgColor?: string;
     className?: string;
 }
 
-const Button: React.FC<ButtonProps> = ({title, type, large, className}) => {
+const Button: React.FC<ButtonProps> = ({title, type, large, bgColor, className}) => {
     return (
         <>
             {
                 type === 'outline' ?
                     <button type="button"
                             className={`gradient-border rounded-4xl ${large ? 'w-full' : 'w-fit'} cursor-pointer ${className || ''}`}>
-                        <div className="bg-white px-3 md:px-7 py-2 md:py-3 rounded-4xl text-nowrap">
+                        <div className={`${bgColor ? bgColor : 'bg-blue-100'} px-3 md:px-7 py-2 md:py-3 rounded-4xl text-nowrap`}>
                             <span className="text-gradient text-xs xl:text-sm leading-4.5 font-semibold">{title}</span>
                         </div>
                     </button> :
